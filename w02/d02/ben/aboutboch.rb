8232089ea1c9504ffe88812798d95814b44683f3
Ben Cochrane <bencochrane1@me.com>
2014-11-19 00:03:52 +1100

    add in full list of students and show them

--- w02/d02/ben/aboutboch.rb
+++ w02/d02/ben/aboutboch.rb
@@ -5,6 +5,76 @@ require 'sinatra/reloader'
 
 
 get '/' do
+
+    @student_data = {
+        "ben" => {
+            name: "Ben Cochrane",
+            skills: ["Rails","Node", "Angular"],
+            tagline: "Heya Garn Matey"
+        },
+       "phil" => {
+            name: "Phil Hancock",
+            skills: ["Javascript","Swift"],
+            tagline: "Hi there"
+        },
+       "michael" => {
+            name: "Michael Foody",
+            skills: ["Javascript","iOS"],
+            tagline: "Heya"
+        },
+       "alan" => {
+            name: "Alan Ng",
+            skills: ["Ruby","Swift"],
+            tagline: "What's up"
+        },
+       "eugene" => {
+            name: "Eugene Lai",
+            skills: ["Github","Rails"],
+            tagline: "Sup"
+        },
+       "ryan" => {
+            name: "Ryan Pauley",
+            skills: ["Python","Javascript"],
+            tagline: "Heya there"
+        },
+       "joel" => {
+            name: "joel hampton",
+            skills: ["AWS","Java"],
+            tagline: "What up!"
+        },
+       "vito" => {
+            name: "vito belgiorno-zegna",
+            skills: ["Ruby","Javascript","Assembler"],
+            tagline: "What up!"
+        },
+       "jonathon" => {
+            name: "Jonathon Hooker",
+            skills: ["AWS","Wordpress"],
+            tagline: "Shup in ya face!"
+        },
+       "frederick" => {
+            name: "Frederick Wong",
+            skills: ["Ruby","Scala"],
+            tagline: "Hey hey!"
+        },
+       "chris" => {
+            name: "Chris Ma",
+            skills: ["Node","Angular"],
+            tagline: "Suppity sup!"
+        }
+    }
+
+    def get_key(hash)
+        keys = []
+        hash.each {|k,v| keys << k }
+        keys
+    end
+
+    @student_names = get_key(@student_data)
+    # @student_name = @student_data[:name]
+    # @student_skills = @student_data[@student_name][:skills]
+    # @student_tagline = @student_data[@student_name][:tagline]
+
     erb :main, layout: :layout
 end
 
@@ -18,25 +88,73 @@ get '/contact' do
     erb :contact, layout: :layout
 end
 
-
-get '/students' do
-    erb :student, layout: :layout
-end
-
 get '/students/:name' do
 
     @student_data = {
         "ben" => {
             name: "Ben Cochrane",
-            skills: ["Ruby","iOS", "Angular"],
+            skills: ["Rails","Node", "Angular"],
             tagline: "Heya Garn Matey"
         },
        "phil" => {
             name: "Phil Hancock",
             skills: ["Javascript","Swift"],
             tagline: "Hi there"
-        }        
+        },
+       "michael" => {
+            name: "Michael Foody",
+            skills: ["Javascript","iOS"],
+            tagline: "Heya"
+        },
+       "alan" => {
+            name: "Alan Ng",
+            skills: ["Ruby","Swift"],
+            tagline: "What's up"
+        },
+       "eugene" => {
+            name: "Eugene Lai",
+            skills: ["Github","Rails"],
+            tagline: "Sup"
+        },
+       "ryan" => {
+            name: "Ryan Pauley",
+            skills: ["Python","Javascript"],
+            tagline: "Heya there"
+        },
+       "joel" => {
+            name: "joel hampton",
+            skills: ["AWS","Java"],
+            tagline: "What up!"
+        },
+       "vito" => {
+            name: "vito belgiorno-zegna",
+            skills: ["Ruby","Javascript","Assembler"],
+            tagline: "What up!"
+        },
+       "jonathon" => {
+            name: "Jonathon Hooker",
+            skills: ["AWS","Wordpress"],
+            tagline: "Shup in ya face!"
+        },
+       "frederick" => {
+            name: "Frederick Wong",
+            skills: ["Ruby","Scala"],
+            tagline: "Hey hey!"
+        },
+       "chris" => {
+            name: "Chris Ma",
+            skills: ["Node","Angular"],
+            tagline: "Suppity sup!"
+        }  
     }
+
+    def get_key(hash)
+        keys = []
+        hash.each {|k,v| keys << k }
+        keys
+    end
+
+    @student_names = get_key(@student_data)
 
     @student_name = params[:name]
     @student_skills = @student_data[@student_name][:skills]
