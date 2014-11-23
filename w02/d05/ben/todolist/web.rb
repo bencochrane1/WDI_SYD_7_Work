6a8af85253edb7902a311460125af99051b61825
Ben Cochrane <bencochrane1@me.com>
2014-11-23 22:38:35 +1100

    used data-mapper database to get the links app to work for me

---
+++ w02/d05/ben/todolist/web.rb
@@ -0,0 +1,55 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'data_mapper'
+require 'json'
+require 'pry'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todolist.db")
+  class Item
+      include DataMapper::Resource
+      property :id, Serial
+      property :title, Text, :required => true
+      property :url, Text, :required => true
+      property :created, DateTime
+  end
+DataMapper.finalize.auto_migrate!
+
+
+get '/' do
+    @items = Item.all
+    redirect '/new' if @items.empty?
+    erb :index
+end
+
+get '/new' do
+    @title = "Add a new Title and URL"
+    erb :new
+end
+
+post '/new' do
+    Item.create(:title => params[:title], :url => params[:url])
+    url = params[:url]
+    title = params[:title]
+    redirect '/'
+end
+
+
+get '/delete/:id' do
+    @item = Item.first(:id => params[:id])
+    erb :delete
+end
+
+post '/delete/:id' do
+    if params.has_key?("ok")
+        item = Item.first(:id => params[:id])
+        item.destroy
+        redirect '/'
+    else
+        redirect '/'
+    end
+end
+
+
+
+
+
